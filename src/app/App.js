e1f0e89bf13eb6cd1cdde19295a289175e1f893f
Salvador Juan Martinez <saljuama@gmail.com>
2019-12-11 01:38:32 +0100

    create react app initial setup

---
+++ src/app/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello World
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
