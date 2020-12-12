7342714ff5ff846a16c7831d0ed67fd02e454d17
saljuama <saljuama@gmail.com>
2020-12-13 00:42:35 +0100

    parse a markdown file from a repo

--- src/app/pages/blog/Blog.js
+++ src/app/pages/blog/Blog.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import ReactMarkdown from 'react-markdown'
 import './Blog.scss'
 
-export const Blog = () =>
-  <h1>Blog page</h1>
+export const Blog = () => {
+  const [article, setArticle] = useState(undefined)
+
+  useEffect(async () => {
+    if (article === undefined) {
+      const response = await fetch('https://raw.githubusercontent.com/saljuama/articles/main/experiment/some-experiment.md')
+      if (response.status === 200) {
+        const text = await response.text()
+        setArticle(text)
+      }
+    }
+  })
+
+  return (
+    <div className='Article'>
+      <ReactMarkdown children={article}/>
+    </div>
+  )
+}
