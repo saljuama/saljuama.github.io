28ef87e2d4b3555bb3c468f6d39a4bbfde7a0108
saljuama <saljuama@gmail.com>
2020-12-13 15:14:33 +0100

    read manually hardcoded articles instead

--- src/app/pages/blog/Blog.js
+++ src/app/pages/blog/Blog.js
@@ -1,21 +1,38 @@
-import React, {useEffect, useState} from 'react'
-import ReactMarkdown from 'react-markdown'
+import React from 'react'
+import {Link, Route, Switch, useParams, useRouteMatch} from 'react-router-dom'
+import {articleUrl, published} from "./published"
 import './Blog.scss'
 
 export const Blog = () => {
-  const [article, setArticle] = useState(undefined)
+  const {path} = useRouteMatch()
+  return (
+    <Switch>
+      <Route exact path={path} component={ArticleList}/>
+      <Route path={`${path}/:category/:article`} component={Article}/>
+    </Switch>
+  )
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://raw.githubusercontent.com/saljuama/articles/main/experiment/some-experiment.md')
-      return response.status === 200 ? await response.text() : ''
-    }
-    setArticle(fetchData())
-  }, [article])
+const ArticleList = () => {
+  const {url} = useRouteMatch()
+  const articles = published.articles
+  return (
+    <div className='Blog'>
+      <h1>Blog main page</h1>
+      {articles.map(article => {
+        const artUrl = articleUrl(article);
+        return <Link to={`${url}${artUrl}`} key={artUrl}>{article.title}</Link>
+      })}
+    </div>
+  )
+}
 
+export const Article = () => {
+  const {category, article} = useParams()
   return (
     <div className='Article'>
-      <ReactMarkdown children={article}/>
+      <h3>{category}</h3>
+      <h3>{article}</h3>
     </div>
   )
 }
---
+++ src/app/pages/blog/published.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const repositoryUrl = `https://raw.githubusercontent.com/saljuama/articles/main`
+
+export const articleUrl = article => `/${article.category}/${article.markdown}`
+
+export const articleMarkdown = async article => await axios
+  .get(`${repositoryUrl}${articleUrl(article)}`)
+  .then(response => response.data)
+  .catch(() => '# Not Found')
+
+export const published = {
+  categories: [
+    {title: 'High Performing Teams', key: 'high-performing-teams'},
+    {title: 'Agile Engineering', key: 'agile-engineering'}
+  ],
+  articles: [
+    {
+      category: 'high-performing-teams',
+      title: 'What is High Performance?',
+      markdown: 'what-is-high-performance?.md'
+    },
+    {
+      category: 'high-performing-teams',
+      title: 'The role of diversity in high performing teams',
+      markdown: 'how-diversity-contributes-to-high-performance.md'
+    },
+    {
+      category: 'agile-engineering',
+      title: 'Feature Toggles and You!',
+      markdown: 'feature-toggles-and-you.md'
+    },
+    {
+      title: 'Breaking it into small pieces',
+      markdown: 'breaking-it-into-small-pieces.md',
+      category: 'agile-engineering'
+    }
+  ]
+}
