13bc51d9b8eee2f8fe826e266c5798366e6e308b
saljuama <saljuama@gmail.com>
2020-12-13 11:23:33 +0100

    wopsie, fix issue with test utils render with store

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {screen} from '@testing-library/react'
-import {renderWithRouter} from "../utils/testUtils";
+import storeForTest from '../config/store'
+import {renderWithReduxAndRouter} from "../utils/testUtils";
 import {App} from './App'
 
 describe('The Application', () => {
   it('renders the landing page', () => {
-    renderWithRouter(<App/>)
+    renderWithReduxAndRouter(<App/>, storeForTest)
     const landingPageTitle = screen.getByText(/Home Page/i)
     expect(landingPageTitle).toBeInTheDocument()
   })
--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,29 +1,30 @@
 import React from 'react'
-import store from '../config/store'
 import {BrowserRouter} from 'react-router-dom'
-import { render } from '@testing-library/react'
-import { Provider } from 'react-redux'
+import {render} from '@testing-library/react'
+import {Provider} from 'react-redux'
 
-/**
- * To be used in components that render inside any `react-router` component for testing
- * @param component - The component to render. Ex: <MyComponent />
- */
-export const renderWithRouter = component =>
-  ({ ...render(<BrowserRouter>{ component }</BrowserRouter>) })
+export const renderWithRouter = component => ({
+  ...render(
+    <BrowserRouter>
+      {component}
+    </BrowserRouter>
+  )
+})
 
-/**
- * To be used in components connected to the `redux` Store
- * @param component - The component to render. Ex: <MyComponent />
- * @param store - The store to use, typically a mocked store
- */
-export const renderWithRedux = (component, store) =>
-  ({ ...render(<Provider store={ store }>{ component }</Provider>) })
+export const renderWithRedux = (component, testStore) => ({
+  ...render(
+    <Provider store={testStore}>
+      {component}
+    </Provider>
+  )
+})
 
-
-/**
- * To be used in components that are connected to the `redux` store and also use `react-router` components
- * @param component - The component to render. Ex: <MyComponent />
- * @param store - The store to use, typically a mocked store
- */
-export const renderWithReduxAndRouter = (component, store = store) =>
-  ({ ...render(<Provider store={ store }><BrowserRouter>{ component }</BrowserRouter></Provider>) })
+export const renderWithReduxAndRouter = (component, testStore) => ({
+  ...render(
+    <Provider store={testStore}>
+      <BrowserRouter>
+        {component}
+      </BrowserRouter>
+    </Provider>
+  )
+})
